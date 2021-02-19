cf7059767aa823d80692e1a01f6e421d51117949
FungGor <33628432+FungGor@users.noreply.github.com>
2021-02-19 15:53:09 +0800

    Add files via upload

--- src/Hello/IncomingMessagesLoggingRunnable.java
+++ src/Hello/IncomingMessagesLoggingRunnable.java
@@ -4,10 +4,14 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Base64;
 
 import javax.bluetooth.RemoteDevice;
 import javax.microedition.io.StreamConnection;
@@ -18,7 +22,8 @@ public class IncomingMessagesLoggingRunnable implements Runnable{
 	 * The Bluetooth connection between this device and the remote one.
 	 */
 	private StreamConnection connection;
-	
+	private String message[] = new String[2];
+	int count = 0;
 	//Initialize the socket connection
 	private Socket socket = null;
 	/**
@@ -42,7 +47,7 @@ public class IncomingMessagesLoggingRunnable implements Runnable{
 	   InputStream input = null;
 	   RemoteDevice device = null;
 	   try {
-		   input = new BufferedInputStream(connection.openInputStream());
+		   input = new BufferedInputStream(connection.openInputStream()); //open the input stream, ready to receive data
 		   device = RemoteDevice.getRemoteDevice(connection);
 	   }catch(IOException e) {
 		System.err.println("Listening service failed. Incoming message won't be displayed.");
@@ -64,23 +69,58 @@ public class IncomingMessagesLoggingRunnable implements Runnable{
 	}
 	    // Main loop of the program: reads a string incoming from the
 	   // Bluetooth connection and prints it.
+	   //https://stackoverflow.com/questions/9231598/how-to-read-all-bytes-together-via-bluetooth
 	   while(true) {
-		   byte buffer[] = new byte[2048];
-		   int bytesRead;
+		   int remining = 0;
+		   int combine = 0;
+		   int bytesRead = 0;
+		   int total = 0;
+		   byte buffer[] = new byte[1024];
+		   String incomingMessage = "";
+		   String reminingMessage = "";
+		   String information = "";
+		   String send = "";
 		   try {
-			   bytesRead = input.read(buffer);
-			   String incomingMessage = new String(buffer,0,bytesRead);
-			   System.out.println("[" + device.getFriendlyName(false) + " - " + device.getBluetoothAddress() + "]: "
-						+ incomingMessage);
-			   
-			   System.out.println("Sending: "+incomingMessage);
-			   String receive = client.echo(incomingMessage);
-			   System.out.println("Receiving: " + receive);
-			  
-			   
+			  if(input.available()>2)
+			  {
+				  if(combine == 0 )
+				  {
+					  bytesRead = input.read(buffer);
+					  incomingMessage = new String(buffer,0,bytesRead);
+					  
+					  if(bytesRead < 16)
+					  {
+						  combine = 1;
+						  while(combine == 1 )
+						  {
+							 remining = input.read(buffer);
+							 reminingMessage = new String(buffer,0,remining);
+							 total = remining + bytesRead;
+							 if(total == 16)
+							 {
+								 information = incomingMessage + reminingMessage;
+								 message[count] = information;
+								 //System.out.println("Message: "+message[count]);
+								 combine = 0;
+								 if(count != 1) {
+									 count = count + 1;
+								 }
+								 else if (count == 1)
+								 {
+									 send = message[0]+message[1];
+									 System.out.println("Sending: "+send);
+									 String receive = client.echo(send);
+									 System.out.println("Receiving: " + receive);
+									 System.out.print("\n");
+									 count = 0;
+								 }
+								 break;
+							 }
+						  }
+					  }
+				  }
+			  }
 		   }catch (IOException e) {
-			     // Don't rethrow this exception so if one message is lost, the
-				// service continues listening.
 			   System.err.println("Error while reading the incoming message.");
 			   e.printStackTrace();
 		   }
